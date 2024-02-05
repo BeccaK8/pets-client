ee6b1f17e1eed6cfe159d8718464695599a2e662
BeccaK8 <150313386+BeccaK8@users.noreply.github.com>
2024-02-05 12:47:08 -0600

    add loading screen and use effect on home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import { getAllPets } from '../api/pet'
+import LoadingScreen from './shared/LoadingScreen'
 
 
 const Home = (props) => {
@@ -15,15 +16,30 @@ const Home = (props) => {
 	// If we want this to run only on the first render and anytime the page refreshes,
 	// we keep the dependency array empty
 	// be careful what you put in the array so you don't cause an infinite loop
+	// useEffect is called RIGHT after the FIRST render of the component
 	useEffect(() => {
 		getAllPets()
-			.then(res => console.log('pets: \n', res.data.pets))
+			// .then(res => console.log('pets from axios: \n', res.data.pets))
+			.then(res => {
+				console.log('use Effect hook ran')
+				setPets(res.data.pets)
+			})
 			.catch(error => console.error) 
 	}, [])
+
+	// WE NEVER EVER DO THIS
+	// getAllPets()
+	//	.then(res => setPets(res.data.pets))
+	// 	.catch(err => console.log(err))
+	// API calls need to happen in an effect hook, or as a result of a singular action (like a form submission)
+	
+	console.log('the pets in Home: \n', pets)
 
 	return (
 		<>
 			<h2>Home Page</h2>
+			{ user != null ? <h5>Hello, { user.email }</h5> : null }
+			{ pets == null ? <LoadingScreen /> : <p>{pets[0].name}</p>}
 		</>
 	)
 }
