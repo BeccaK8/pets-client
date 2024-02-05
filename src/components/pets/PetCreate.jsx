2aa0e46eaec259063961a2119d33f1b9c90f3ea7
BeccaK8 <150313386+BeccaK8@users.noreply.github.com>
2024-02-05 15:52:12 -0600

    add petform and handle change for petform

--- src/components/pets/PetCreate.jsx
+++ src/components/pets/PetCreate.jsx
@@ -11,6 +11,8 @@
 
 import { useState } from 'react'
 
+import PetForm from '../shared/PetForm'
+
 const PetCreate = (props) => {
     // pull out our props
     const { user, msgAlert } = props
@@ -23,10 +25,54 @@ const PetCreate = (props) => {
         adoptable: false
     })
 
+    const onChange = (evt) => {
+        // evt is the placeholder for the event
+        // evt.persist() handles the virtual dom because React uses the virtual dom 
+        // we want our form data to persist every time the page renders, which will be a lot of times
+        evt.persist()
+
+        // if you pass an argument to the callback function of your state hook updater/setter
+        // that argument is a placeholder for the most recent state
+        // this will maintain our state, anything you have typed before the  next letter
+        setPet( prevPet => {
+            const updatedName = evt.target.name
+            let updatedValue = evt.target.value
+
+            // the above two items work great for strings
+            // however we need to handle numbers and booleans as well
+            if (evt.target.type === 'number') {
+                // if the target is a number, parse integers from the value
+                updatedValue = parseInt(evt.target.value)
+            }
+
+            // to handle checkbox, we need to tell it when to send true and when to send false
+            // because the default values for a checkbox are 'checked' or 'unchecked'
+            // we need to convert those to the appropriate boolean value
+            if (updatedName === 'adoptable' && evt.target.checked) {
+                updatedValue = true
+            } else if (updatedName === 'adoptable' && !evt.target.checked) {
+                updatedValue = false
+            }
+
+            // this will actually build our pet object
+            // we grab an attribute name and assign the respective value
+            const updatedPet = { [updatedName] : updatedValue }
+
+            // to keep all the old stuff and add the newly typed letters, numbers, etc.
+            return {
+                ...prevPet, ...updatedPet
+            }
+        })
+    }
+
+    console.log('the pet inside create: \n', pet)
     return (
-        <>
-            <h1>Create Pet Component</h1>
-        </>
+        <PetForm 
+            pet={pet}
+            handleChange={onChange}
+            handleSubmit={() => {console.log('handle submit')}}
+            heading="Add a New Pet"
+        />
     )
 }
 
